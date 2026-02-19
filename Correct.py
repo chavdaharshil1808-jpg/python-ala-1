fcea31e66db67ff3b4bc6b87490e04f6392874d4
harshil chavda <chavdaharshil1808@gmail.com>
2026-02-19 03:23:39 -0800

    Update Correct.py

--- Correct.py
+++ Correct.py
@@ -1,16 +1,22 @@
 print("Car Rental")
+
 cars = ["Sedan","SUV","Mini"]
 rent = 1000
+
 car = input("Enter car type: ")
-days = input("Enter days: ")
+days = int(input("Enter days: "))
+
 if car in car:
     total = days * rent
 else:
     print("Car not available")
+
 tax = total * 0.1
 amount = total + tax
+
 print("Car:", car)
 print("Amount:", amount)
+
 for i in range(3):
     print("Thank you")
 
